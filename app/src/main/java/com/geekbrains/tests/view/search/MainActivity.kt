8f1171474764808259dcc775c8ec822d7662dc3f
Stambulo Anatolii <stambulo@yandex.ru>
2021-06-16 16:43:39 +0300

    Homework-5

--- app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -2,8 +2,6 @@ package com.geekbrains.tests.view.search
 
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.geekbrains.tests.BuildConfig
@@ -37,33 +35,26 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
         toDetailsActivityButton.setOnClickListener {
             startActivity(DetailsActivity.getIntent(this, totalCount))
         }
-        setQueryListener()
-        setRecyclerView()
-    }
 
-    private fun setRecyclerView() {
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = adapter
-    }
-
-    private fun setQueryListener() {
-        searchEditText.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val query = searchEditText.text.toString()
-                if (query.isNotBlank()) {
+        searchButton.setOnClickListener {
+            val query = searchEditText.text.toString()
+            if (query.isNotBlank()) {
                     presenter.searchGitHub(query)
-                    return@OnEditorActionListener true
                 } else {
                     Toast.makeText(
                         this@MainActivity,
                         getString(R.string.enter_search_word),
                         Toast.LENGTH_SHORT
                     ).show()
-                    return@OnEditorActionListener false
                 }
-            }
-            false
-        })
+        }
+
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
     }
 
     private fun createRepository(): RepositoryContract {
